b2894028e1db9a1c19000c51c97d8af49a883758
emmabastas <emma.bastas@protonmail.com>
2023-02-14 23:23:17 +0100

    Add profile events endpoint

--- heroku_app.py
+++ heroku_app.py
@@ -1,7 +1,5 @@
 from typing import *
 import os
-#import sys
-import json
 from flask import Flask
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
@@ -18,7 +16,7 @@ def hello_world():
 
 
 @app.route("/event/<int:event_id>", methods=["GET"])
-def event(event_id: int):
+def event(event_id: int) -> facebook.GenericEvent:
 
     d = get_webdriver()
 
@@ -31,6 +29,31 @@ def event(event_id: int):
     generic_event = facebook.parse_event_details(event_details)
 
     return generic_event
+
+
+@app.route("/profile/<profile_name>/events", methods=["GET"])
+def profile_events(profile_name: str) -> List[facebook.GenericEvent]:
+
+    d = get_webdriver()
+
+    _, postrender = util.memoize(
+        "fb_page_events_for_profile",
+        util.curry(facebook.fetch_page__events_for_profile)(d)
+    )(profile_name)
+
+    ids = facebook.extract_event_ids(postrender)
+
+    events = []
+    for id in ids:
+        event_page = util.memoize(
+            "fb_page_event",
+            util.curry(facebook.fetch_page__event)(d)
+        )(id)
+        events.append(facebook.extract_event_details(event_page))
+
+    generic_events = [facebook.parse_event_details(e) for e in events]
+
+    return generic_events
 
 
 def get_webdriver() -> WebDriver:
